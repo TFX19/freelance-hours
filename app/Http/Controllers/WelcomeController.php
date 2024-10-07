134899d468ee2441812fee53eeb5aa5d5565d9bc
Tiago Borges <tiagoc.borges02@gmail.com>
2024-10-07 16:09:09 +0100

    wip

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,10 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('welcome');
+
+        $user = User::query()->create([
+            'name' => 'Tiago',
+            'email' => 'tiago@email.com',
+            'password'=> 'tiago',
+        ]);
+
+        dd($user);
+
+        //1:15:50
+
+        return view('teste.tiago');
     }
 }
